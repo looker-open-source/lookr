ed8f1def97173c6733c1a0edb2180b2c5e8386df
Mike DeAngelo <drstrangelove@google.com>
2021-10-27 16:43:04 -0400

    Added Rd (document files)

--- tests/testthat/test.ApiSettings.R
+++ tests/testthat/test.ApiSettings.R
@@ -2,6 +2,6 @@ context("ApiSettings tests")
 library(lookr)
 
 test_that("can initialize", {
-  settings <- ApiSettings$new(configFile = "~/looker-sdk/looker.ini")
+  settings <- ApiSettings$new(configFile = "~/lookr/looker.ini")
   expect_is(settings, "ApiSettings")
 })
