825eef08ed823a6215f485f001818cb42a9d5c30
Mike DeAngelo <drstrangelove@google.com>
2021-10-28 13:23:58 -0400

    Reduce the number of test environments, fix looker.ini location.

--- tests/testthat/test.UserSession.R
+++ tests/testthat/test.UserSession.R
@@ -2,6 +2,6 @@ context("UserSession tests")
 library(lookr)
 
 test_that("can initialize", {
-  settings <- UserSession$new(configFile = "~/lookr/looker.ini")
+  settings <- UserSession$new(configFile = "looker.ini")
   expect_is(settings, "UserSession")
 })
